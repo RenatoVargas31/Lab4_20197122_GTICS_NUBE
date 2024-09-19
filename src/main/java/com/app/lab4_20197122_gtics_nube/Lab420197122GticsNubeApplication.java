dd68ee4158bb9ff687e1ea120685589ee315966b
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-09-19 06:47:18 -0500

    Initial commit

---
+++ src/main/java/com/app/lab4_20197122_gtics_nube/Lab420197122GticsNubeApplication.java
@@ -0,0 +1,13 @@
+package com.app.lab4_20197122_gtics_nube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420197122GticsNubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420197122GticsNubeApplication.class, args);
+    }
+
+}
